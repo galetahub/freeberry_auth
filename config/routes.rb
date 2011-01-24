640ac4296212947c5fba0103b5ad715ca691c161
Pavlo <superp1987@gmail.com>
2011-01-24 06:13:32 -0800

    add auth logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   post '/auth/accounts', :to => 'freeberry_auth/accounts#create', :as => :auth_accounts
   post '/auth/vkontakte', :to => 'freeberry_auth/accounts#vkontakte', :as => :auth_vkontakte
+  get '/auth/logout', :to => 'freeberry_auth/accounts#logout', :as => :auth_logout
 end
